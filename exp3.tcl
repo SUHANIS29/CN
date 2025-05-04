45c55e3887a582013d157a3efbdccc4b3dd67fb6
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2025-05-04 14:33:47 +0530

    exp3.tcl

--- exp3.tcl
+++ exp3.tcl
@@ -1,55 +1,40 @@
 set ns [new Simulator]
 
 set nf [open out.nam w]
-
 $ns namtrace-all $nf
 
 proc finish {} {
-
-global ns nf
-
-$ns flush-trace
-
-close $nf
-
-exec nam out.nam &
-
-exit 0
-
+    global ns nf
+    $ns flush-trace
+    close $nf
+    exec nam out.nam &
+    exit 0
 }
 
 set n0 [$ns node]
-
 set n1 [$ns node]
 
-$ns duplex-link $n0 $n1 1kb 10ms DropTail
+$ns duplex-link $n0 $n1 1Kb 10ms DropTail
 
 set tcp0 [new Agent/TCP]
-
 $ns attach-agent $n0 $tcp0
 
 set sink0 [new Agent/TCPSink]
-
 $ns attach-agent $n1 $sink0
 
 $ns connect $tcp0 $sink0
 
 set cbr0 [new Application/Traffic/CBR]
-
 $cbr0 set packetSize_ 500
-
 $cbr0 set interval_ 0.005
-
 $cbr0 attach-agent $tcp0
 
 set ftp0 [new Application/FTP]
-
 $ftp0 attach-agent $tcp0
 
 $ns at 0.5 "$ftp0 start"
-
 $ns at 4.5 "$ftp0 stop"
 
-$ns at 5.5 "finish"
+$ns at 5.0 "finish"
 
 $ns run
